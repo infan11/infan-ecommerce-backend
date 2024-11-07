587aeb8fe742627df1cb075f7fbfd67091af4482
infan11 <infanjiounrahman20606@gmail.com>
2024-11-07 18:51:51 +0600

    added addItem add Books

--- index.js
+++ index.js
@@ -49,6 +49,24 @@ async function run() {
       console.log(result);
       res.send(result)
     })
+    app.delete("/users/:id" , async (req , res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+    // user admin 
+    app.patch("/users/admin/:id" , async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc  = {
+        $set : {
+          role : "admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter , updatedDoc)
+      res.send(result);
+    })
     // Add items api
     app.get('/addItems' , async (req ,res) => {
       const result = await addItemsCollection.find().toArray()
